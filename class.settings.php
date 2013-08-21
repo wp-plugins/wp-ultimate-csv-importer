6135d61095d0d0a84006b1433b75bfda1f578cf3
smackcoders <smackcoders@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-21 10:22:32 +0000

    Stable version 3.2.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-ultimate-csv-importer/trunk@759842 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- class.settings.php
+++ class.settings.php
@@ -1,49 +1,65 @@
 <?php
-class Settings{
+class IMPSettings
+{
 
-	public $activePlugins = array();
+    public $activePlugins = array();
 
-	function __construct(){
-		$this->activePlugins = get_option('active_plugins');
-	}
+    function __construct()
+    {
+        $this->activePlugins = get_option('active_plugins');
+    }
 
-	public function chkCustomPostPlugin(){
-		$customPostType = $this->chkCustomTypePost();
-		$cctm = $this->chkCCTM();
-		if( ($cctm) || ($customPostType))
-			return true;
-		else
-			return false;
-	}
+    public function chkCustomPostPlugin()
+    {
+        $customPostType = $this->chkCustomTypePost();
+        $cctm = $this->chkCCTM();
+        if (($cctm) || ($customPostType))
+            return true;
+        else
+            return false;
+    }
 
-	public function chkCCTM(){
-		if(in_array('custom-content-type-manager/index.php',$this->activePlugins))
-			return true;
-		else
-			return false;
-	}
+    public function chkCCTM()
+    {
+        if (in_array('custom-content-type-manager/index.php', $this->activePlugins))
+            return true;
+        else
+            return false;
+    }
 
-	public function chkCustomTypePost(){
-		if(in_array('custom-post-type-ui/custom-post-type-ui.php',$this->activePlugins))
-			return true;
-		else
-			return false;
-	}
+    public function chkCustomTypePost()
+    {
+        if (in_array('custom-post-type-ui/custom-post-type-ui.php', $this->activePlugins))
+            return true;
+        else
+            return false;
+    }
 
-	public function isPluginPresent($plugin){
-		$plugins = get_plugins();
-		$pluginName = array();
-		foreach($plugins as $plug => $key)
-			$pluginName[] = $plug;
-		if(in_array($plugin,$pluginName))
-			return true;
-		else
-			return false;
-	}
-	public function isPluginActive($plugin){
-		if(in_array($plugin,$this->activePlugins))
-			return true;
-		else
-			return false;
-	}
+    public function isPluginPresent($plugin)
+    {
+        $plugins = get_plugins();
+        $pluginName = array();
+        foreach ($plugins as $plug => $key)
+            $pluginName[] = $plug;
+        if (in_array($plugin, $pluginName))
+            return true;
+        else
+            return false;
+    }
+
+    public function isPluginActive($plugin)
+    {
+        if (in_array($plugin, $this->activePlugins))
+            return true;
+        else
+            return false;
+    }
+
+    /**
+     * Get Saved Settings
+     */
+    function getSettings()
+    {
+        return get_option('wpcsvprosettings');
+    }
 }
