b886734ae5c972bc624caf0b231230331fecda04
smackcoders <smackcoders@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-12 09:35:04 +0000

    Version 3.6.2 Security fix release
    git-svn-id: https://plugins.svn.wordpress.org/wp-ultimate-csv-importer/tags/3.6.2@964451 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/skinnymvc/core/Model.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * filename:    Model.php
+ * description: Use this class for methods that you want to extend into all model classes.
+ */
+
+abstract class SkinnyModel extends SkinnyBaseModel {
+}
+    
---
+++ modules/categories/actions/actions.php
@@ -0,0 +1,56 @@
+<?php
+/*********************************************************************************
+ * WP Ultimate CSV Importer is a Tool for importing CSV for the Wordpress
+ * plugin developed by Smackcoder. Copyright (C) 2014 Smackcoders.
+ *
+ * WP Ultimate CSV Importer is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License version 3
+ * as published by the Free Software Foundation with the addition of the
+ * following permission added to Section 15 as permitted in Section 7(a): FOR
+ * ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY WP Ultimate
+ * CSV Importer, WP Ultimate CSV Importer DISCLAIMS THE WARRANTY OF NON
+ * INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * WP Ultimate CSV Importer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses or write
+ * to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ * You can contact Smackcoders at email address info@smackcoders.com.
+ *
+ * The interactive user interfaces in original and modified versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License
+ * version 3, these Appropriate Legal Notices must retain the display of the
+ * WP Ultimate CSV Importer copyright notice. If the display of the logo is
+ * not reasonably feasible for technical reasons, the Appropriate Legal
+ * Notices must display the words
+ * "Copyright Smackcoders. 2014. All rights reserved".
+ ********************************************************************************/
+
+class CategoriesActions extends SkinnyActions {
+
+    public function __construct()
+    {
+    }
+
+  /**
+   * The actions index method
+   * @param array $request
+   * @return array
+   */
+    public function executeIndex($request)
+    {
+        // return an array of name value pairs to send data to the template
+        $data = array();
+        return $data;
+    }
+
+}
